a8d564654949954046c1b45a06dd30adde65c534
Zia <ipad12305@hotmail.com>
2020-08-28 14:10:53 -0400

    Files

---
+++ Image Converter (8-bit index)/Image Converter (8-bit index)/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Image_Converter__8_bit_index_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.indexLabel = new System.Windows.Forms.Label();
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 308);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(402, 212);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(418, 38);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(212, 482);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "earth.png";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 225);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Picture Name:";
+            // 
+            // indexLabel
+            // 
+            this.indexLabel.AutoSize = true;
+            this.indexLabel.Location = new System.Drawing.Point(418, 22);
+            this.indexLabel.Name = "indexLabel";
+            this.indexLabel.Size = new System.Drawing.Size(79, 13);
+            this.indexLabel.TabIndex = 5;
+            this.indexLabel.Text = "Defined Colors:";
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(300, 292);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(69, 13);
+            this.sizeLabel.TabIndex = 6;
+            this.sizeLabel.Text = "Picture Size: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 292);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Picture Array:";
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(337, 10);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 25);
+            this.loadButton.TabIndex = 8;
+            this.loadButton.Text = "Load Picture";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(11, 526);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(520, 50);
+            this.calculateButton.TabIndex = 9;
+            this.calculateButton.Text = "Calculate Picture";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(92, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "8-Bit Index";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 50);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(174, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(66, 17);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Print Full";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Pixel Color:  ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Options:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.sizeLabel);
+            this.Controls.Add(this.indexLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Picture 8-bit Indexed Hex Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label indexLabel;
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Image Converter (8-bit index)/Image Converter (8-bit index)/Form1.cs
@@ -0,0 +1,223 @@
+﻿//Upload image to debug folder and write the name in text box
+
+//Program automatically resizes image to 106x49
+//Program goes through each pixel, and stores it in pixelColor array
+//All the unique colors are put into the colorIndex list
+//Then each pixel is assigned a number corrosponding to the location of the
+//color in the colorIndex list
+//Both colorIndex list and pixelColorRef array are printed out
+
+//Make sure to put image through an 8-bit color indexer to 
+//ensure that there are no more than 256 unique colors in image
+//and to resize to 106x49 first:
+//https://online-converting.com/image/convert2png/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Image_Converter__8_bit_index_
+{
+    public partial class Form1 : Form
+    {
+        const int picXSize = 41;   //X-size of image
+        const int picYSize = 23;   //Y-size of image (I have 46 LEDS)
+        bool error = false;
+        Bitmap pictureMap;
+        Image pic;
+        Bitmap zoomMap;
+        string textBoxText = "";
+        bool checkBoxState = false;
+
+        //Output strings (used to speed up program)
+        string indexOutput;     
+        string arrayOutput;
+
+        //Arrays and lists
+        IList<Color> colorIndex = new List<Color>();            //Stores all unique colors
+        Color[,] pixelColor = new Color[picXSize, picYSize];    //Stores all pixel colors
+        int[,] pixelColorRef = new int[picXSize, picYSize];     //Stores all pixel colors by refrencing colors in colorIndex
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Calculate Button
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            CreatePictureBitmap();
+
+            if (!error)
+            {
+                richTextBox1.Clear();
+
+                //Goes through each pixel on image
+                for (int x = 0; x < pictureMap.Width; x++)
+                {
+
+                    for (int y = 0; y < pictureMap.Height; y++)
+                    {
+                        pixelColor[x, y] = pictureMap.GetPixel(x, y);                   //Stores pixel color
+
+                        if (!colorIndex.Contains(pixelColor[x, y]))                     //Checks if the pixel color are exists in colorIndex
+                            colorIndex.Add(pixelColor[x, y]);                           //If not, adds it to colorIndex
+
+                        pixelColorRef[x, y] = colorIndex.IndexOf(pixelColor[x, y]);     //Adds a reference to the pixel color in the pixelColorRef array
+                    }
+                }
+
+                indexLabel.Text = "Defined Colors: " + colorIndex.Count();
+                sizeLabel.Text = "Picture Size: " + pictureMap.Width.ToString() + " x " + pictureMap.Height.ToString();
+                PrintColorIndex();
+                PrintPixelColorRef();
+            }
+        }
+
+        //Load button
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            CreatePictureBitmap();
+        }
+
+        //Creates bitmap of image, resizes it and displays it on picturebox
+        private void CreatePictureBitmap()
+        {
+            try
+            {
+                if (textBox1.Text != textBoxText || checkBoxState != checkBox1.Checked)                     //runs only if a change was made to pic name or checkbox state
+                {
+                    pic = Image.FromFile(Application.StartupPath + @"\Images\" + textBox1.Text, true);      //Stores image in pic variable
+                    pictureMap = new Bitmap(pic, new Size(picXSize, picYSize));                             //Resizes the image to the set size   
+
+                    if (checkBox1.Checked)
+                        pictureMap = pictureMap.Clone(new Rectangle(0, 0, pictureMap.Width, pictureMap.Height), PixelFormat.Format8bppIndexed);                   //This indexes the picture but it's not as good as the website
+
+                    zoomMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);      //Makes a new bitmap zooming in image (for picturebox)
+
+                    //This whole thing zooms the map properly and makes sure to keep the resolution the same
+                    using (var gr = Graphics.FromImage(zoomMap))
+                    {
+                        gr.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;;
+                        gr.DrawImage(pictureMap, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+                    }
+
+                    pictureBox1.Image = zoomMap;
+
+                    //This resets all the labels, textboxes, and variables
+                    indexLabel.Text = "Defined Colors: ";
+                    sizeLabel.Text = "Picture Size: ";
+                    richTextBox1.Clear();
+                    richTextBox2.Clear();
+                    colorIndex.Clear();
+                    textBoxText = textBox1.Text;
+                    checkBoxState = checkBox1.Checked;
+
+                    error = false;
+                }
+            }
+            catch (Exception)
+            {
+                error = true;
+                MessageBox.Show("There was an error. Check the path to the image file.");
+            }
+        }
+
+        //Goes through the colorIndex list and prints it to textbox2 with the appropriate format to work with c++
+        private void PrintColorIndex()
+        {
+            richTextBox2.Clear();
+
+            if (checkBox2.Checked)
+            {
+                indexOutput = $"const long int clr[][{colorIndex.Count()}] = ";
+                indexOutput += "\n{";
+            }
+            else
+                indexOutput = "{";
+
+            for (int i = 0; i < colorIndex.Count; i++)
+            {
+                if (i == colorIndex.Count - 1)
+                {
+                    indexOutput += "0x" + ColorTranslator.ToHtml(colorIndex[i]).Substring(1, 6) + "}";
+                    if (checkBox2.Checked)
+                        indexOutput += ";";
+                }
+                else
+                    indexOutput += "0x" + ColorTranslator.ToHtml(colorIndex[i]).Substring(1, 6) + ", ";
+            }
+
+            richTextBox2.Text = indexOutput;
+        }
+
+        //Goes through pixelColorRef and prints it to textbox1 with the appropriate format to work with c++
+        private void PrintPixelColorRef()
+        {
+            richTextBox1.Clear();
+            if (checkBox2.Checked)
+            {
+                arrayOutput = $"const byte pixel[][{picXSize}][{picYSize}] PROGMEM = ";
+                arrayOutput += "{{";
+            }
+            else
+                arrayOutput = "{";
+
+            for (int x = 0; x < pixelColorRef.GetLength(0); x++)
+            {
+                arrayOutput += "{";
+
+                for (int y = 0; y < pixelColorRef.GetLength(1); y++)
+                {
+                    if (y == pixelColorRef.GetLength(1) - 1)
+                        arrayOutput += pixelColorRef[x, y];                //Prints out the same color reference twice to get the proportions correct
+                    else
+                        arrayOutput += pixelColorRef[x, y] + ", ";
+                }
+
+                if (x == pixelColorRef.GetLength(0) - 1)
+                {
+                    arrayOutput += "}}";
+                    if (checkBox2.Checked)
+                        arrayOutput += "};";
+                }
+                else
+                    arrayOutput += "}, ";
+            }
+
+            richTextBox1.Text = arrayOutput;
+        }
+
+        //Copy button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(indexOutput + "\n" + arrayOutput); //Copies the outputs to clipboard in correct format to paste in c++
+        }
+
+        //This is for getting color by hovering over a pixel on map
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                MouseEventArgs me = (MouseEventArgs)e;
+                Point coordinates = me.Location;
+                int x = map(coordinates.X, 0, 400, 0, 41);
+                int y = map(coordinates.Y, 0, 225, 0, 23);
+                label3.Text = "Pixel Color: " + ColorTranslator.ToHtml(pictureMap.GetPixel(x, y));
+            }
+        }
+
+        //Mapping number function
+        private static int map(int value, int fromLow, int fromHigh, int toLow, int toHigh)
+        {
+            return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+        }
+    }
+}
